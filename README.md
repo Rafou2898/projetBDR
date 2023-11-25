40aefe05aabbc5cc71f99daa796cea00f1510aec
Rafou98 <95037521+Rafou28@users.noreply.github.com>
2023-11-25 13:50:29 +0100

    Creations de la plus part des fichiers

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # projetBDR
+
+## Description
+
+## Correction effectué
+
+### Corrigé de l'UML
+
+1er UML
+![1erUML](images/1st_UML.png)
---
+++ codes/create_DB.sql
@@ -0,0 +1,54 @@
+CREATE TABLE personnage (
+  nom VARCHAR(50) PRIMARY KEY,
+  hp SMALLINT,
+  exp SMALLINT,
+  argent SMALLINT
+);
+
+CREATE TABLE metier (
+
+);
+
+CREATE TABLE recettes (
+
+);
+CREATE TABLE ressources (
+
+);
+
+CREATE TABLE prix (
+
+);
+CREATE TABLE marche (
+
+);
+CREATE TABLE objets (
+
+);
+CREATE TABLE consommables (
+
+);
+CREATE TABLE equipement (
+
+);
+CREATE TABLE  (
+  
+);
+ CREATE TABLE  (
+  
+);
+ CREATE TABLE  (
+  
+);
+ CREATE TABLE  (
+  
+);
+ CREATE TABLE  (
+  
+);
+ 
+-- Add foreign key constraints
+ALTER TABLE `Table1`
+ADD CONSTRAINT `fk_constraint_name` FOREIGN KEY (`foreign_key_column`) REFERENCES `Table2`(`id`);
+
+-- Continue creating tables and adding relationships as needed
